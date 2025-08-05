946082c36be5c4694d187f1375dd4133d477b153
Raja Muhammad Arbab <rajaarbabthakur1@gmail.com>
2025-08-05 14:53:31 +0500

    Update urls.py

--- project/fileupload_project/urls.py
+++ project/fileupload_project/urls.py
@@ -19,16 +19,3 @@ urlpatterns += [
 })),
 
 ]
-
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', include('filemanager.urls')),  # ✅ This connects the root path to filemanager
-# ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
