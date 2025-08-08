13005a14352e35811d40382e730ab4e85e1255c4
Raja Muhammad Arbab <rajaarbabthakur1@gmail.com>
2025-08-08 13:01:08 +0500

    Delete project directory

--- project/filemanager/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import upload_file, list_files, download_file
-
-urlpatterns = [
-    path('upload/', upload_file, name='upload-file'),
-    path('files/', list_files, name='list-files'),
-    path('files/<str:filename>', download_file, name='download-file'),
-]
